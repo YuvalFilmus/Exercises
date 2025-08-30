d3e3cb73a9d88bdc8ea36f9d508d97ed6d5d6867
Yuval Filmus <filmus.yuval@gmail.com>
2025-08-30 17:11:48 +0300

    One new problem

--- Exercises/Infi2-Solutions.lean
+++ Exercises/Infi2-Solutions.lean
@@ -24,7 +24,45 @@ lemma infi1_targil2_problem1 : Irrational (Real.sqrt 3) := by
     norm_num
   case neg hb =>
     push_neg at hb
-    sorry
+    set g := Int.gcd a b with hg
+    set a' := a/g with ha'
+    set b' := b/g with hb'
+    have hab' : Int.gcd a' b' = 1 := by
+      rw [ha', hb', hg]
+      rwa [Int.gcd_ediv_gcd_ediv_gcd_of_ne_zero_right]
+    suffices Real.sqrt 3 ≠ a'/b' by
+      contrapose! this
+      rw [ha', hb', this]
+      rw [Int.cast_div, Int.cast_div]
+      rw [div_div_div_cancel_right₀ (by aesop)]
+      · exact Int.gcd_dvd_right a b
+      · aesop
+      · exact Int.gcd_dvd_left a b
+      · aesop
+    by_contra! h
+    have : 3 * (b')^2 = (a')^2 := by
+      have : √3 * √3 = (a' / b') * (a' / b') := by rw [h]
+      rw [←sq, sq_sqrt (by norm_num)] at this
+      rify
+      rw [this, sq, sq]
+      have : (b' : ℝ) ≠ 0 := by aesop
+      field_simp
+    have ha3 : 3 ∣ a' := by
+      apply Prime.dvd_of_dvd_pow (by norm_num)
+      exact Dvd.intro (b'^2) this
+    have hb3 : 3 ∣ b' := by
+      apply Prime.dvd_of_dvd_pow (by norm_num)
+      obtain ⟨c, hc⟩ := dvd_def.mp ha3
+      rw [hc] at this
+      apply Dvd.intro (c^2)
+      suffices 3 * (3 * c^2) = 3 * (b')^2 by
+        apply Int.eq_of_mul_eq_mul_left (show 3 ≠ 0 by norm_num)
+        assumption
+      rw [this]
+      ring
+    have : 3 ∣ Int.gcd a' b' := Int.dvd_gcd ha3 hb3
+    rw [hab'] at this
+    norm_num at this
 
 lemma infi1_targil2_problem2 {x : ℝ}
   (hnonneg : 0 ≤ x) (hsmall : ∀ ε > 0, x < ε) : x = 0 := by
